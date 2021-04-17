c699d1d98bd1e125f9307c3bb7bd03bf8b41e0f4
Jacob Brown <Jake@insphereltd.com>
2021-04-17 16:44:33 +0100

    Added continous output of ASCII 'J' in min prog to test ACIA uart functionality

--- asm/src/min.asm
+++ asm/src/min.asm
@@ -19,11 +19,26 @@ A1_CONTROL      =   ACIA1+3     ; Control register
 .org $C000
 
 Main:
+    LDA	A1_STATUS		    ; Read 6551 status
+	AND	#$10			    ; Is tx buffer full?
+	BEQ	Main	            ; if not, loop back
+    LDA #'J'                ; Load and sent out ASCII 'J'
+    STA A1_DATA
     JMP Main
+
+SetupACIA1:
+    STA A1_STATUS           ; Software reset
+    LDA #%00001011          ; Enable DTR, disable receive IRQ, disable transmit IRQ
+    STA A1_COMMAND          ; Disable echo mode, disable parity
+    LDA #%00011111          ; Setup intenal divider Baud control @ 19,200 Baud
+    STA A1_CONTROL          ; 1 Stop Bit, 8 Data Bits
+    RTS
 
 Reset:
     LDX #$FF
     TXS
+    JSR SetupACIA1
+    CLI                     ; enable interupts
     JMP Main
 
 IRQ:
